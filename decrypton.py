0915e79df5b6a17dbb7f8414f0bdc9c16d69a9df
Not-My-Problem <116504083+Not-My-Problem@users.noreply.github.com>
2023-05-25 20:26:05 +0200

    Version 0.0.1 is here!
    Its still a test but it works perfectly in next updates i plan to add graphic interface.

---
+++ decrypton.py
@@ -0,0 +1,47 @@
+from cryptography.fernet import Fernet
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("decrypon_key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+
+    return open("decrypon_key.key", "rb").read()
+
+def encrypt_file(filename, key):
+
+    fernet = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = fernet.encrypt(file_data)
+    with open(filename, "wb") as encrypted_file:
+        encrypted_file.write(encrypted_data)
+
+def decrypt_file(filename, key):
+
+    fernet = Fernet(key)
+    with open(filename, "rb") as encrypted_file:
+        encrypted_data = encrypted_file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(filename, "wb") as decrypted_file:
+        decrypted_file.write(decrypted_data)
+
+try:
+    key = load_key()
+except FileNotFoundError:
+    print("Key not found... Generating new key...")
+    generate_key()
+    key = load_key()
+
+filename = input("File name: ")
+
+mode = input("Choose mode:\n1. Encrypting\n2. Decrypting\n")
+if mode == "1":
+    encrypt_file(filename, key)
+    print("File has been encrypted.")
+elif mode == "2":
+    decrypt_file(filename, key)
+    print("File has been decrypted")
+else:
+    print("Choose 1 or 2")
